de6dedae03363aaaa02404776deb79b96fa85f31
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2025-04-18 22:23:28 -0400

    package girl-technology

---
+++ nixos/packages/girl-technology/package.nix
@@ -0,0 +1,14 @@
+{ fetchFromGitHub, rustPlatform }:
+rustPlatform.buildRustPackage {
+  pname = "girl_technology";
+  version = "0.3.0";
+
+  src = fetchFromGitHub {
+    owner = "p0lyw0lf";
+    repo = "girl.technology";
+    rev = "632efc130d0c3e90c06d6c397e72c9a841d8f046";
+    hash = "sha256-13MI9J5LaJloQy+8YhyB8w1cZpMZKNfS3eTc1oKs9uw=";
+  };
+
+  cargoHash = "sha256-rsXjq3hYhJlanpgzWxJzGH1MJ78yb4qRD92L8j7rP0k=";
+}
